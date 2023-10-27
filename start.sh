071a6782145599b7a1dcbd315deaec13aaf9df8b
Camilo <camilogastondinardo@hotmail.com>
2023-10-27 10:28:04 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 sudo docker container stop material-kit-react
 sudo docker container rm material-kit-react
 
-#Correr el container
+# Correr el container
 sudo docker-compose -f docker-compose.prod.yml up --build -d
 # Mensaje del que contenedor paro
 echo "material-kit-react esta corriendo"
